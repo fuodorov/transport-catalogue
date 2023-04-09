1731903e4e7eaaf986dd0038e1b6458af3d4b6bf
Vyacheslav Fedorov <slava@fuodorov.ru>
2023-04-09 13:48:03 +0000

    Update renderer

--- transport-catalogue/renderer.cpp
+++ transport-catalogue/renderer.cpp
@@ -11,10 +11,12 @@ namespace renderer {
         screen_ = screen;
         return *this;
     }
+
     Visualization& Visualization::SetLineWidth(double width) {
         line_width_ = width;
         return *this;
     }
+
     Visualization& Visualization::SetStopRadius(double radius) {
         stop_radius_ = radius;
         return *this;
@@ -42,6 +44,7 @@ namespace renderer {
         max_lat_(catalogue_.GetMaxStopCoordinates().lat),
         zoom_(CalculateZoom()) {}
 
+    
     void MapImageRenderer::Render() {
         PutRouteLines();
         PutRouteNames();
@@ -50,104 +53,97 @@ namespace renderer {
     }
 
     void MapImageRenderer::PutRouteLines() {
-        const double& width = settings_.line_width_;
-
-        int route_id{0};
-        bool is_previous_route_empty{true};
+        int id{0};
+        bool is_empty{true};
 
         for (std::string_view bus_name : catalogue_.GetOrderedBusList()) {
             auto [bus, stops] = catalogue_.GetRouteInfo(bus_name);
-
-            route_id = is_previous_route_empty ? route_id : route_id + 1;
-
+            id = is_empty ? id : id + 1;
+            
             svg::Polyline route;
-            for (const auto& stop : stops)
+            for (const auto& stop : stops){
                 route.AddPoint(ToScreenPosition(stop->point));
+            }
 
-            image_.Add(route.SetStrokeColor(TakeColorById(route_id))
+            image_.Add(route.SetStrokeColor(TakeColorById(id))
                         .SetFillColor("none"s)
-                        .SetStrokeWidth(width)
+                        .SetStrokeWidth(settings_.line_width_)
                         .SetStrokeLineCap(svg::StrokeLineCap::ROUND)
                         .SetStrokeLineJoin(svg::StrokeLineJoin::ROUND));
 
-            is_previous_route_empty = stops.empty();
+            is_empty = stops.empty();
         }
     }
 
     void MapImageRenderer::PutRouteNames() {
-        const auto& bus_settings = settings_.labels_.at(LabelType::Bus);
-        const auto& under_layer_settings = settings_.under_layer_;
-
-        int route_id{0};
-        bool is_previous_route_empty{true};
+        int id{0};
+        bool is_empty{true};
 
         for (std::string_view bus_name : catalogue_.GetOrderedBusList()) {
             auto [bus, stops] = catalogue_.GetFinalStops(bus_name);
-
-            route_id = is_previous_route_empty ? route_id : route_id + 1;
-
-            if (stops.empty())
+            id = is_empty ? id : id + 1;
+            
+            if (stops.empty()){
                 continue;
+            }
 
             for (const auto& stop : stops) {
                 image_.Add(svg::Text()
                             .SetData(bus->number)
-                            .SetFillColor(under_layer_settings.color_)
-                            .SetStrokeColor(under_layer_settings.color_)
-                            .SetStrokeWidth(under_layer_settings.width_)
+                            .SetFillColor(settings_.under_layer_.color_)
+                            .SetStrokeColor(settings_.under_layer_.color_)
+                            .SetStrokeWidth(settings_.under_layer_.width_)
                             .SetStrokeLineCap(svg::StrokeLineCap::ROUND)
                             .SetStrokeLineJoin(svg::StrokeLineJoin::ROUND)
                             .SetPosition(ToScreenPosition(stop->point))
-                            .SetOffset(bus_settings.offset_)
-                            .SetFontSize(bus_settings.font_size_)
+                            .SetOffset(settings_.labels_.at(LabelType::Bus).offset_)
+                            .SetFontSize(settings_.labels_.at(LabelType::Bus).font_size_)
                             .SetFontFamily(constants::DEFAULT_FONT_FAMILY)
                             .SetFontWeight(constants::DEFAULT_FONT_WEIGHT));
 
                 image_.Add(svg::Text()
                             .SetData(bus->number)
                             .SetPosition(ToScreenPosition(stop->point))
-                            .SetOffset(bus_settings.offset_)
-                            .SetFontSize(bus_settings.font_size_)
+                            .SetOffset(settings_.labels_.at(LabelType::Bus).offset_)
+                            .SetFontSize(settings_.labels_.at(LabelType::Bus).font_size_)
                             .SetFontFamily(constants::DEFAULT_FONT_FAMILY)
                             .SetFontWeight(constants::DEFAULT_FONT_WEIGHT)
-                            .SetFillColor(TakeColorById(route_id)));
+                            .SetFillColor(TakeColorById(id)));
             }
 
-            is_previous_route_empty = stops.empty();
+            is_empty = stops.empty();
         }
     }
 
     void MapImageRenderer::PutStopCircles() {
-        for (const auto& [_, stop] : catalogue_.GetAllStopsFromRoutes())
+        for (const auto& [_, stop] : catalogue_.GetAllStopsFromRoutes()){
             image_.Add(svg::Circle()
                         .SetCenter(ToScreenPosition(stop->point))
                         .SetRadius(settings_.stop_radius_)
                         .SetFillColor("white"s));
+        }
     }
 
     void MapImageRenderer::PutStopNames() {
-        const auto& stop_settings = settings_.labels_.at(LabelType::Stop);
-        const auto& under_layer_settings = settings_.under_layer_;
-
         for (const auto& [_, stop] : catalogue_.GetAllStopsFromRoutes()) {
             image_.Add(svg::Text()
                         .SetData(stop->name)
-                        .SetFillColor(under_layer_settings.color_)
-                        .SetStrokeColor(under_layer_settings.color_)
-                        .SetStrokeWidth(under_layer_settings.width_)
+                        .SetFillColor(settings_.under_layer_.color_)
+                        .SetStrokeColor(settings_.under_layer_.color_)
+                        .SetStrokeWidth(settings_.under_layer_.width_)
                         .SetStrokeLineCap(svg::StrokeLineCap::ROUND)
                         .SetStrokeLineJoin(svg::StrokeLineJoin::ROUND)
                         .SetPosition(ToScreenPosition(stop->point))
-                        .SetOffset(stop_settings.offset_)
-                        .SetFontSize(stop_settings.font_size_)
+                        .SetOffset(settings_.labels_.at(LabelType::Stop).offset_)
+                        .SetFontSize(settings_.labels_.at(LabelType::Stop).font_size_)
                         .SetFontFamily(constants::DEFAULT_FONT_FAMILY));
 
             image_.Add(svg::Text()
                         .SetData(stop->name)
                         .SetFillColor("black"s)
                         .SetPosition(ToScreenPosition(stop->point))
-                        .SetOffset(stop_settings.offset_)
-                        .SetFontSize(stop_settings.font_size_)
+                        .SetOffset(settings_.labels_.at(LabelType::Stop).offset_)
+                        .SetFontSize(settings_.labels_.at(LabelType::Stop).font_size_)
                         .SetFontFamily(constants::DEFAULT_FONT_FAMILY));
         }
     }
@@ -157,19 +153,19 @@ namespace renderer {
         const auto [min_lat, min_lng] = catalogue_.GetMinStopCoordinates();
         const auto [max_lat, max_lng] = catalogue_.GetMaxStopCoordinates();
 
-        auto make_zoom_coefficient = [&](double max_min_diff, double screen_size) -> double {
-            return (max_min_diff == 0.) ? std::numeric_limits<double>::max() : (screen_size - 2. * settings_.screen_.padding_) / max_min_diff;
+        auto get_zoom = [&](double diff, double screen_size) -> double {
+            return (diff == 0.) ? std::numeric_limits<double>::max() : (screen_size - 2. * settings_.screen_.padding_) / diff;
         };
 
-        double zoom_x = make_zoom_coefficient(max_lng - min_lng, settings_.screen_.width_);
-        double zoom_y = make_zoom_coefficient(max_lat - min_lat, settings_.screen_.height_);
+        double zoom_x = get_zoom(max_lng - min_lng, settings_.screen_.width_);
+        double zoom_y = get_zoom(max_lat - min_lat, settings_.screen_.height_);
 
         zoom = std::min(zoom_x, zoom_y);
         return (zoom == std::numeric_limits<double>::max()) ? 0. : zoom;
     }
 
-    svg::Color MapImageRenderer::TakeColorById(int route_id) const {
-        return settings_.colors_.at(static_cast<int>(route_id) % settings_.colors_.size());
+    svg::Color MapImageRenderer::TakeColorById(int id) const {
+        return settings_.colors_.at(static_cast<int>(id) % settings_.colors_.size());
     }
 
     svg::Point MapImageRenderer::ToScreenPosition(geo::Coordinates position) {
