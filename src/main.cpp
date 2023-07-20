3545ca30203dca0b26c3cdffbc4aad58b97b13cd
Vyacheslav Fedorov <slava@fuodorov.ru>
2023-07-20 12:22:22 +0000

    add src for sprint 15

---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+#include <fstream>
+#include <iostream>
+
+#include "json_reader.h"
+#include "request_handler.h"
+
+using namespace std;
+
+using namespace transport_catalogue;
+using namespace transport_catalogue::detail::json;
+using namespace transport_catalogue::detail::router;
+
+using namespace map_renderer;
+using namespace request_handler;
+using namespace serialization;
+
+void PrintUsage(std::ostream& stream = std::cerr) {
+    stream << "Usage: transport_catalogue [make_base|process_requests]\n"sv;
+}
+
+int main(int argc, char* argv[]) {
+    
+    if (argc != 2) {
+        PrintUsage();
+        return 1;
+    }
+
+    const std::string_view mode(argv[1]);
+    
+    TransportCatalogue transport_catalogue;   
+    
+    RenderSettings render_settings;
+    RoutingSettings routing_settings;
+    
+    SerializationSettings serialization_settings;
+    
+    JSONReader json_reader;
+    vector<StatRequest> stat_request;   
+    
+    if (mode == "make_base"sv) {
+        
+        json_reader = JSONReader(cin); 
+        
+        json_reader.parse_node_make_base(transport_catalogue, 
+                                         render_settings, 
+                                         routing_settings, 
+                                         serialization_settings);
+        
+        ofstream out_file(serialization_settings.file_name, ios::binary);    
+        catalogue_serialization(transport_catalogue, render_settings, routing_settings, out_file);
+        
+    } else if (mode == "process_requests"sv) {
+        
+        json_reader = JSONReader(cin);    
+        
+        json_reader.parse_node_process_requests(stat_request, 
+                                                serialization_settings);
+        
+        ifstream in_file(serialization_settings.file_name, ios::binary); 
+        
+        Catalogue catalogue = catalogue_deserialization(in_file);
+            
+        RequestHandler request_handler;       
+        
+        request_handler.execute_queries(catalogue.transport_catalogue_, 
+                                        stat_request, 
+                                        catalogue.render_settings_,
+                                        catalogue.routing_settings_);
+        
+        print(request_handler.get_document(), cout); 
+        
+    } else {
+        PrintUsage();
+        return 1;
+    }
+}
---
+++ src/request_handler.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "transport_catalogue.h"
+#include "map_renderer.h"
+#include "json_builder.h"
+#include "transport_router.h"
+
+using namespace transport_catalogue;
+using namespace map_renderer;
+using namespace transport_catalogue::detail::json;
+using namespace transport_catalogue::detail::json::builder;
+using namespace transport_catalogue::detail::router;
+
+namespace request_handler {
+
+class RequestHandler {
+public:
+           
+    RequestHandler() = default;
+    
+    std::optional<RouteInfo> get_route_info(std::string_view start, 
+                                            std::string_view end, 
+                                            TransportCatalogue& catalogue, 
+                                            TransportRouter& routing) const;
+    
+    std::vector<geo::Coordinates> get_stops_coordinates(TransportCatalogue& catalogue_) const;
+    std::vector<std::string_view> get_sort_buses_names(TransportCatalogue& catalogue_) const;
+    
+    BusQueryResult bus_query(TransportCatalogue& catalogue, std::string_view str);
+    StopQueryResult stop_query(TransportCatalogue& catalogue, std::string_view stop_name);
+    
+    Node execute_make_node_stop(int id_request, const StopQueryResult& query_result);
+    Node execute_make_node_bus(int id_request, const BusQueryResult& query_result);
+    Node execute_make_node_map(int id_request, TransportCatalogue& catalogue, RenderSettings render_settings);
+    Node execute_make_node_route(StatRequest& request, TransportCatalogue& catalogue, TransportRouter& routing);
+    
+    void execute_queries(TransportCatalogue& catalogue, 
+                         std::vector<StatRequest>& stat_requests, 
+                         RenderSettings& render_settings,
+                         RoutingSettings& route_settings);
+    
+    void execute_render_map(MapRenderer& map_catalogue, TransportCatalogue& catalogue_) const;
+       
+    const Document& get_document();
+
+private:
+    Document doc_out;
+};
+    
+}//end namespace request_handler
